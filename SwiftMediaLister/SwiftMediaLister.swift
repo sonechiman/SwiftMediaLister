8fed3dbab9d3546d570c4bfc0860e57a7269cd4f
Yuya Soneoka <ej.a9p96@gmail.com>
2015-08-12 23:03:05 +0900

    Complete option logic

--- SwiftMediaLister/SwiftMediaLister.swift
+++ SwiftMediaLister/SwiftMediaLister.swift
@@ -14,27 +14,45 @@ public class SwiftMediaLister{
     
     let library = ALAssetsLibrary()
     var result:[[String: AnyObject]] = []
+    var option: [String: AnyObject] = [:]
     
-    public init(){
+    public init(){}
+    
+    public func readLibrary(thumbnail: Bool = true, limit: Int = 20, mediaTypes: [String] = ["image"], offset:Int = 0){
+        option = ["thumbnail": thumbnail, "limit":limit , "mediaTypes": mediaTypes, "offset": offset]
+        loadMedia(option)
     }
+
+    
     
     // TODO:cordova対応
-    public func test(){
-        library.enumerateGroupsWithTypes(ALAssetsGroupSavedPhotos,
-            usingBlock: {
-                (group: ALAssetsGroup!, stop: UnsafeMutablePointer) in
-                if group != nil{
-                    println(group)
-                    // TODO: optionで切り替える
-                    let filter = ALAssetsFilter.allPhotos()
-                    group.enumerateAssetsUsingBlock{
-                        (asset:ALAsset!, id:Int , stopp: UnsafeMutablePointer) in
-                        if asset != nil{
-                            self.result.append(self.setDictionary(asset, id: id))
-                        }
-                    }
-                 println(self.result)
+    public func loadMedia(option: [String: AnyObject]){
+        library.enumerateGroupsWithTypes(ALAssetsGroupSavedPhotos,usingBlock: {
+            (group: ALAssetsGroup!, stop: UnsafeMutablePointer) in
+            if group == nil{
+                return
+            }
+            
+            if let filter = self.getFilter(option["mediaTypes"] as! [String]){
+                group.setAssetsFilter(filter)
+            } else {
+                return
+            }
+            
+            let num = group.numberOfAssets()
+            let indexSet = self.getIndexSet(num, limit: option["limit"] as! Int, offset: option["offset"] as! Int)
+            if indexSet == nil{
+                return
+            }
+            
+            group.enumerateAssetsAtIndexes(indexSet!, options: NSEnumerationOptions.Reverse){
+                (asset:ALAsset!, id:Int , stop: UnsafeMutablePointer) in
+                if asset != nil{
+                    self.result.append(self.setDictionary(asset, id: id, option:option))
                 }
+            }
+            
+            println(self.result)
             }, failureBlock:{
                 (myerror: NSError!) -> Void in
                 println("error occurred: \(myerror.localizedDescription)")
@@ -43,7 +61,7 @@ public class SwiftMediaLister{
     }
     
     // TODO: Location等を追加可能
-    func setDictionary(asset: ALAsset, id: Int) -> [String: AnyObject]{
+    func setDictionary(asset: ALAsset, id: Int, option: [String: AnyObject]) -> [String: AnyObject]{
         var data: [String: AnyObject] = [:]
         data["id"] = id
         data["mediaType"] = setType(asset)
@@ -57,7 +75,9 @@ public class SwiftMediaLister{
         data["height"] = rep.dimensions().height
         data["wigth"] = rep.dimensions().width
         data["mimeType"] = UTTypeCopyPreferredTagWithClass(rep.UTI(), kUTTagClassMIMEType).takeUnretainedValue()
-        data["thumbnailPath"] = saveThumbnail(asset, id: id)
+        if (option["thumbnail"] as! Bool) {
+            data["thumbnailPath"] = saveThumbnail(asset, id: id)
+        }
         return data
     }
     
@@ -70,7 +90,6 @@ public class SwiftMediaLister{
         let filePath = cacheDirPath.stringByAppendingPathComponent("\(id).jpeg")
         
         if imageData.writeToFile(filePath, atomically: true){
-            println(filePath)
             return filePath
         } else {
             println("error occured: Cannot save thumbnail image")
@@ -86,5 +105,29 @@ public class SwiftMediaLister{
             return "video"
         }
         return  ""
+    }
+    
+    // TODO: Add music and playlist and audio
+    func getFilter(mediaTypes: [String]) -> ALAssetsFilter?{
+        if contains(mediaTypes, "image"){
+            if contains(mediaTypes, "video"){
+                return ALAssetsFilter.allAssets()
+            } else {
+                return ALAssetsFilter.allPhotos()
+            }
+        } else if contains(mediaTypes, "video"){
+            return ALAssetsFilter.allVideos()
+        }
+        return nil
+    }
+    
+    private func getIndexSet(max: Int, limit:Int, offset: Int) -> NSIndexSet?{
+        if offset >= max{
+            return nil
+        } else if offset + limit > max{
+            return NSIndexSet(indexesInRange: NSMakeRange(0, max - offset))
+        } else {
+            return NSIndexSet(indexesInRange: NSMakeRange(max - offset - limit, limit))
+        }
     }
 }
